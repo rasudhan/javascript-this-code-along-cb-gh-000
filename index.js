ae57630592c8f33e3360ae2e29f7f21a963d9baa
Anandha Sudhan <rasudhan@gmail.com>
2018-10-08 20:26:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,73 @@
 const app = "I don't do much."
+function Sandwich(bread, ingredients, name) {
+  this.bread = bread;
+  this.ingredients = ingredients;
+  this.name = name;
+}
+ function serve(customer) {
+  console.log("Hey " + customer + ", here's your " + this.name + ", enjoy!");
+}
+ function serve() {
+    if(arguments.length > 0) {
+        var customers = Array.prototype.slice.call(arguments);
+        last = customers.pop();
+        console.log(this.name + " for " + customers.join(", ") + " and " + last + ". Enjoy!");
+    }else {
+        console.log(this.name + ". Order up!");
+    }
+}
+ var gc = new Sandwich("white", ["cheese"], "Grilled Cheese");
+var pbj = new Sandwich("wheat", ["peanut butter", "raspberry jam"],
+"Peanut Butter & Jelly");
+/*new example*/
+ function Sandwich(bread, ingredients, name) {
+  this.bread = bread;
+  this.ingredients = ingredients;
+  this.name = name;
+  this.describe = function() {
+    console.log("Your " + this.name + " includes: " + this.ingredients.join(", ") + ". Yum!");
+  }
+}
+
+var pbj = new Sandwich("wheat", ["chunky peanut butter", "blackberry preserves"], "PB&Jam");
+
+pbj.describe();
+ var salad = {
+  ingredients: ["croutons", "romaine hearts", "steak", "parmesan", "caesar dressing"],
+  name: "Steak Caesar"
+}
+ pbj.describe.call(salad);
+ var describeSalad = pbj.describe.bind(salad);
+describeSalad();
+ /*new example*/
+function Sandwich(bread, ingredients, name) {
+  this.bread = bread;
+  this.ingredients = ingredients;
+  this.name = name;
+  this.describe = function() {
+    console.log("Your " + this.name + " includes: " + this.ingredients.join(", ") + ". Yum!");
+  }
+}
+
+var pbj = new Sandwich("wheat", ["chunky peanut butter", "blackberry preserves"], "PB&Jam");
+
+var salad = {
+  ingredients: ["croutons", "romaine hearts", "steak", "parmesan", "caesar dressing"],
+  name: "Steak Caesar"
+}
+
+salad.describe = pbj.describe.bind(salad);
+/*new example*/
+function visitTable() {
+  console.log("The server is visiting " + this.name + " at table number " + this.tableNumber);
+}
+ function Customer(name, tableNumber) {
+  this.name = name;
+  this.tableNumber = tableNumber;
+}
+ //create new Customer instance
+var sally = new Customer("Sally", "4");
+
+//schedule table visit
+var visitSally = visitTable.bind(sally);
+setTimeout(visitSally, 1000); 
