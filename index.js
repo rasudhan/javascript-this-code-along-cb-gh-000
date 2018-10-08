a35510a9247226c78f0e73e2ce483be639cf0af7
Anandha Sudhan <rasudhan@gmail.com>
2018-10-08 20:26:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,9 +28,9 @@ var pbj = new Sandwich("wheat", ["peanut butter", "raspberry jam"],
     console.log("Your " + this.name + " includes: " + this.ingredients.join(", ") + ". Yum!");
   }
 }
- 
+
 var pbj = new Sandwich("wheat", ["chunky peanut butter", "blackberry preserves"], "PB&Jam");
- 
+
 pbj.describe();
  var salad = {
   ingredients: ["croutons", "romaine hearts", "steak", "parmesan", "caesar dressing"],
@@ -48,14 +48,14 @@ function Sandwich(bread, ingredients, name) {
     console.log("Your " + this.name + " includes: " + this.ingredients.join(", ") + ". Yum!");
   }
 }
- 
+
 var pbj = new Sandwich("wheat", ["chunky peanut butter", "blackberry preserves"], "PB&Jam");
- 
+
 var salad = {
   ingredients: ["croutons", "romaine hearts", "steak", "parmesan", "caesar dressing"],
   name: "Steak Caesar"
 }
- 
+
 salad.describe = pbj.describe.bind(salad);
 /*new example*/
 function visitTable() {
@@ -67,7 +67,7 @@ function visitTable() {
 }
  //create new Customer instance
 var sally = new Customer("Sally", "4");
- 
+
 //schedule table visit
 var visitSally = visitTable.bind(sally);
 setTimeout(visitSally, 1000); 
